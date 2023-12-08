d9a2d8ba2315546db6da84a5d8b7a2e7c3cbb52c
Michael Ekstrand <md@ekstrandom.net>
2023-12-07 20:53:19 -0500

    get meter working

--- src/view/meter.rs
+++ src/view/meter.rs
@@ -1,3 +1,89 @@
 //! Horizontal bar meter widget.
+use std::borrow::Cow;
+use std::cmp::min;
+
+use ratatui::prelude::*;
+use ratatui::widgets::Widget;
 
 // Note: Unicode bars start at U+2588 (full bar).
+const BLOCK_CHAR: char = '\u{2588}';
+
+pub struct Meter {
+    label: Cow<'static, str>,
+    value: f32,
+}
+
+impl Meter {
+    pub fn new<S: Into<Cow<'static, str>>>(label: S) -> Meter {
+        Meter {
+            label: label.into(),
+            value: f32::NAN,
+        }
+    }
+
+    pub fn value(self, value: f32) -> Meter {
+        Meter { value, ..self }
+    }
+}
+
+impl Widget for Meter {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let layout = Layout::new()
+            .direction(Direction::Horizontal)
+            .constraints([
+                Constraint::Length(self.label.len() as u16 + 1),
+                Constraint::Min(8),
+            ])
+            .split(area);
+        let l = layout[0];
+        buf.set_string(l.x, l.y, self.label, Style::new().bold());
+
+        let b = layout[1];
+        buf.get_mut(b.x, b.y).symbol = "[".into();
+        buf.get_mut(b.x + b.width - 1, b.y).symbol = "]".into();
+
+        let txt = format!("{:.1}%", self.value * 100.0);
+        let color = if self.value >= 0.8 {
+            Color::Red
+        } else if self.value >= 0.6 {
+            Color::Yellow
+        } else {
+            Color::Green
+        };
+
+        let space = b.width - 2;
+        let bw = (space * 8) as f32 * self.value;
+        let bw = bw.floor() as u32;
+        let blocks = bw / 8;
+        let partial = bw % 8;
+        let tlen = txt.len() as u32;
+        let bmax = space as u32 - tlen;
+
+        let mut bar = String::with_capacity(space as usize);
+        for _ in 0..min(blocks, bmax) {
+            bar.push(BLOCK_CHAR);
+        }
+        if blocks <= bmax && partial > 0 {
+            bar.push(char::from_u32(BLOCK_CHAR as u32 + 9 - partial).expect("invalid block char"));
+        }
+        buf.set_string(b.x + 1, b.y, &bar, Style::new().fg(color));
+        buf.set_string(
+            b.x + 1 + space - tlen as u16,
+            b.y,
+            &txt,
+            Style::new().fg(color),
+        );
+        if blocks > bmax {
+            let tbw = blocks - bmax;
+            buf.set_style(
+                Rect {
+                    x: bmax as u16,
+                    y: b.y,
+                    width: tbw as u16,
+                    height: 1,
+                },
+                Style::new().fg(Color::White).bg(color),
+            );
+        }
+    }
+}
--- src/view/quicklook.rs
+++ src/view/quicklook.rs
@@ -9,31 +9,35 @@ use sysinfo::{CpuExt, SystemExt};
 
 use crate::SystemState;
 
+use super::meter::Meter;
+
 pub(super) fn render_quicklook(frame: &mut Frame, state: &SystemState, area: Rect) -> Result<()> {
     let cpu_usage = state.system.global_cpu_info().cpu_usage();
     let mem_usage = state.system.used_memory() as f32 / state.system.total_memory() as f32;
     let swap_usage = state.system.used_swap() as f32 / state.system.total_swap() as f32;
-    let chart = BarChart::default()
-        .direction(Direction::Horizontal)
-        .bar_gap(0)
-        .max(1000)
-        .data(
-            BarGroup::default().bars(&[
-                Bar::default()
-                    .label(Line::from("CPU"))
-                    .value((cpu_usage * 10.0) as u64)
-                    .text_value(format!("{:.1}%", cpu_usage))
-                    .style(Style::new().fg(Color::Green)),
-                Bar::default()
-                    .label(Line::from("MEM"))
-                    .value((mem_usage * 1000.0) as u64)
-                    .text_value(format!("{:.1}%", mem_usage * 100.0)),
-                Bar::default()
-                    .label(Line::from("SWP"))
-                    .value((swap_usage * 1000.0) as u64)
-                    .text_value(format!("{:.1}%", swap_usage * 100.0)),
-            ]),
-        );
-    frame.render_widget(chart, area);
+    frame.render_widget(
+        Meter::new("CPU").value(cpu_usage / 100.0),
+        Rect {
+            y: area.y + 1,
+            height: 1,
+            ..area
+        },
+    );
+    frame.render_widget(
+        Meter::new("MEM").value(mem_usage),
+        Rect {
+            y: area.y + 2,
+            height: 1,
+            ..area
+        },
+    );
+    frame.render_widget(
+        Meter::new("SWP").value(swap_usage),
+        Rect {
+            y: area.y + 3,
+            height: 1,
+            ..area
+        },
+    );
     Ok(())
 }
