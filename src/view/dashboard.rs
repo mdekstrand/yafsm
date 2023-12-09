b9f137807d1ef4ee066c17e43fc32165a3251436
Michael Ekstrand <md@ekstrandom.net>
2023-12-09 08:51:49 -0500

    factor dashbaord

---
+++ src/view/dashboard.rs
@@ -0,0 +1,27 @@
+//! Primary dashboard view.
+
+use anyhow::Result;
+use ratatui::prelude::*;
+
+use crate::{backend::MonitorBackend, model::MonitorState};
+
+use super::banner::render_banner;
+use super::quicklook::render_quicklook;
+
+pub fn render_dashboard<B>(frame: &mut Frame, state: &MonitorState<B>) -> Result<()>
+where
+    B: MonitorBackend,
+{
+    let layout = Layout::new()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(1),
+            Constraint::Length(1),
+            Constraint::Min(3),
+            Constraint::Min(0),
+        ])
+        .split(frame.size());
+    render_banner(frame, state, layout[0])?;
+    render_quicklook(frame, state, layout[2])?;
+    Ok(())
+}
--- src/view/mod.rs
+++ src/view/mod.rs
@@ -5,9 +5,10 @@ use ratatui::prelude::*;
 
 use crate::{backend::MonitorBackend, MonitorState};
 
-use self::{banner::render_banner, quicklook::render_quicklook};
+use self::dashboard::render_dashboard;
 
 mod banner;
+mod dashboard;
 mod meter;
 mod quicklook;
 mod util;
@@ -16,16 +17,5 @@ pub fn render_screen<B>(frame: &mut Frame, state: &MonitorState<B>) -> Result<()
 where
     B: MonitorBackend,
 {
-    let layout = Layout::new()
-        .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Length(1),
-            Constraint::Length(1),
-            Constraint::Min(3),
-            Constraint::Min(0),
-        ])
-        .split(frame.size());
-    render_banner(frame, state, layout[0])?;
-    render_quicklook(frame, state, layout[2])?;
-    Ok(())
+    render_dashboard(frame, state)
 }
